ae4880a14e63c56ec6531911c99163c50bc45ee7
rorogant70 <ronagurl19@gmail.com>
2017-08-25 10:38:32 -0400

    Update and rename .gitignore to mirror.py

---
+++ .gitignore/mirror.py
@@ -0,0 +1,23 @@
+
+lemon=["lemonle"]
+pineapple=["pineapple"]
+
+
+print ('Hello, welcome to Mirror Words, type lemonle or pineapple to start')
+
+def mirror(reflect):
+    reflect = [::-1]
+
+
+  If input = lemonle;
+    print (reflect.lemon[5,6])
+    return true
+
+  elif input = pineapple;
+    print ('pineapple')
+    return false
+
+def newlemon();
+    lemon[0] == lemon[6]
+    lemon[1] == lemon[5]
+    
